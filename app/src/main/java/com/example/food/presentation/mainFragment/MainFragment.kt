074ae58a6c7bf3fe4acf7a0c2edf553fd5a208da
Лачимов Егор Владимирович <55237892+iDuckk@users.noreply.github.com>
2022-10-14 23:12:17 +0700

    Version 2
    create CollapsingToolbarLayout

--- app/src/main/java/com/example/food/presentation/mainFragment/MainFragment.kt
+++ app/src/main/java/com/example/food/presentation/mainFragment/MainFragment.kt
@@ -5,56 +5,68 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.CenterInside
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.food.R
+import com.example.food.databinding.FragmentMainBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [mainFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class mainFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding: FragmentMainBinding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initSpinner()
+
+        initSalesImages()
+
+    }
+
+    private fun initSalesImages() {
+        Glide
+            .with(this)
+            .load(R.drawable.sale3)
+            .override(900,270)
+            .transform(CenterInside(), RoundedCorners(25))
+            .into(binding.imSale1)
+
+        Glide
+            .with(this)
+            .load(R.drawable.sale4)
+            .override(900,270)
+            .transform(CenterInside(), RoundedCorners(25))
+            .into(binding.imSale2)
+    }
+
+    private fun initSpinner(){
+        val cities = resources.getStringArray(R.array.Cities)
+        val adapter = ArrayAdapter(requireContext(),
+            android.R.layout.simple_spinner_item, cities)
+        binding.spinnerCity.adapter = adapter
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment mainFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            mainFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
