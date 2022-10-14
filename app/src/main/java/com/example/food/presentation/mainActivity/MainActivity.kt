54c45b49908719eea4adf751cd9cc961114d9f9c
Лачимов Егор Владимирович <55237892+iDuckk@users.noreply.github.com>
2022-10-14 14:20:37 +0700

    Version 1
    create mainFragment.kt

--- app/src/main/java/com/example/food/presentation/mainActivity/MainActivity.kt
+++ app/src/main/java/com/example/food/presentation/mainActivity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.food
+package com.example.food.presentation.mainActivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.food.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
